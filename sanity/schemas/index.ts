edd5b9ffdcbe18a935271e874a4a41b84c486fb6
KIRAN KUMAR <kitusmart@gmail.com>
2026-01-29 13:27:02 +0530

    fix: permanently remove care-guide folder and sync footer

--- sanity/schemas/index.ts
+++
@@ -1,9 +0,0 @@
-// sanity/schemas/index.ts
-
-import { categoryType } from "./categoryType";
-import { customerType } from "./customerType";
-import { orderType } from "./orderType";
-import { productType } from "./productType";
-
-// This is a NAMED export
-export const schemaTypes = [categoryType, customerType, orderType, productType];
